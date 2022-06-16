17788ba85a47dbf4b0abe655cf2ced76cf13c408
Cameron Stephenson <camstephenson2017@gmail.com>
2022-06-16 05:19:35 -0400

    Layout has been added.

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -4,6 +4,7 @@ import React from 'react';
 import HeaderComponent from './partials/HeaderComponent';
 import FooterComponent from './partials/FooterComponent';
 // End import components
+// anther comment to commit
 
 const DefaultLayout = ({children}) => {
   return (
